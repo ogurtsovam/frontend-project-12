d762928ea62ac33fca062357b451b9e04d53cec2
ogurtsovam <ogurts.maria@gmail.com>
2025-07-26 17:22:40 +0300

    README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ogurtsovam/frontend-project-12/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ogurtsovam/frontend-project-12/actions)
+
+### Messanger App
+[Open App](https://frontend-project-12-cmq9.onrender.com)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,46 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: './dist',
+  },
+//  root: path.resolve(__dirname),
+//   resolve: {
+//     alias: {
+//       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+//     }
+//   },
+//   server: {
+//     port: 8080,
+//     hot: true
+//   },
+
+  
+
   server: {
     port: 5002,
     proxy: {
+      // Проксируем запросы к API
       '/api': {
         target: 'http://localhost:5001',
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+        ],
       },
     },
   },
